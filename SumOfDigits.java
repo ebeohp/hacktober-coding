bb43b20a589e873803421b6806f04719151655f5
Phoebe Lin <phoebe1318@yahoo.com>
2020-10-05 10:21:11 -0500

    fixed comment errors

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -5,7 +5,7 @@ class SumOfDigits{
    public static void main(String[] args) throws IOException{
       //Take in row of numbers separated by spaces
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      //Change numbers to int and add into an arrayList
+      //Add numbers as strings into a list
       System.out.print("Enter numbers with spaces between them here ==> ");
       String nums[] = br.readLine().split(" ");
       //Loop through all numbers and add
